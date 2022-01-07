8b968128f374c21a3baa72bb1d058f154e3b9a6f
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2022-01-07 18:03:23 -0300

    feat: start project

---
+++ src/components/gift.jsx
@@ -0,0 +1,16 @@
+import styles from '../styles/gift.module.css'
+
+const Gift = () => {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}>
+
+      </div>
+      <div className={styles.body}>
+
+      </div>
+    </div>
+  )
+}
+
+export default Gift
