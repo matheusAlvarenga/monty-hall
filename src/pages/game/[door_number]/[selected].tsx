5529d6efc2cd90880d335d711ff45f78be796c9e
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2022-01-07 20:58:49 -0300

    feat: create game page

---
+++ src/pages/game/[door_number]/[selected].tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import Door from "../../../components/door"
+import Gift from "../../../components/gift"
+import { changeDoorState, createDoorsArray } from "../../../functions/doors"
+import DoorModel from "../../../model/door"
+import styles from '../../../styles/game.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const Game = () => {
+  const {query: { door_number, selected }} = useRouter()
+
+  const [doors, setDoors] = useState([])
+
+  useEffect(() => {
+    setDoors(createDoorsArray(+door_number, +selected))
+  },[door_number, selected])
+
+  const renderDoors = () => {
+    return doors.map((door: DoorModel) => <Door key={door.number} door={door} onChange={(door) => setDoors(changeDoorState(doors, door))}></Door>)
+  }
+
+  return (
+    <div className={styles.game}>
+      <div className={styles.doors}>
+        {renderDoors()}
+      </div>
+      <div className={styles.buttons}>
+        <Link href="/">
+          <button>Restart Game</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Game
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,7 @@ import { changeDoorState, createDoorsArray } from "../functions/doors";
 import DoorModel from "../model/door";
 
 export default function Home() {
-  const [doors, setDoors] = useState(createDoorsArray(3, 2))
-
-  const renderDoors = () => {
-    return doors.map((door: DoorModel) => <Door key={door.number} door={door} onChange={(door) => setDoors(changeDoorState(doors, door))}></Door>)
-  }
-
   return (
-    <div>
-      <Gift></Gift>
-      {renderDoors()}
-    </div>
+    <h1>Form</h1>
   )
 }
