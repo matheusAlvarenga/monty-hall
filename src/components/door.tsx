4962ffe58931b69b2569f4116a8354e0bfd57e07
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2022-01-07 18:58:01 -0300

    feat: integrate door to doormodel

--- src/components/door.tsx
+++ src/components/door.tsx
@@ -5,13 +5,17 @@ interface DoorProps {
   door: DoorModel
 }
 
-const Door = (props) => {
+const Door = ({ door }: DoorProps) => {
   return (
     <div className={styles.area}>
-      <div className={`${styles.frame} ${props.selected ? styles.selected : ''}`}>
+      <div className={`${styles.frame} ${door.selected ? styles.selected : ''}`}>
         <div className={styles.door}>
-          <div className={`${styles.number} ${props.selected ? styles.selected_number : ''}`}>3</div>
-          <div className={`${styles.knob} ${props.selected ? styles.selected_knob : ''}`}></div>
+          <div className={
+            `${styles.number} ${door.selected ? styles.selected_number : ''}`
+          }>
+            {door.number}
+            </div>
+          <div className={`${styles.knob} ${door.selected ? styles.selected_knob : ''}`}></div>
         </div>
       </div>
       <div className={styles.floor}></div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Door from "../components/door";
 import Gift from "../components/gift";
+import DoorModel from "../model/door";
 
 export default function Home() {
+  const [door1, setDoor1] = useState(new DoorModel(1))
+
   return (
     <div>
       <Gift></Gift>
-      <Door></Door>
-      <Door selected></Door>
+      <Door door={door1}></Door>
     </div>
   )
 }
